a1d01a448f29c6bad75d04d96a575ea7c7eff1a7
Armyz <armyz@foxmail.com>
2017-07-16 21:54:29 +0800

    修改客户端

--- learnPythonExample/019-tcp客户端.py
+++ learnPythonExample/019-tcp客户端.py
@@ -4,7 +4,7 @@ from socket import *
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
-clientSocket.connect(("127.0.0.1", 7788))
+clientSocket.connect(("47.93.57.111", 7788))
 
 while True:
     sendMsg = raw_input("请输入发送内容:")
