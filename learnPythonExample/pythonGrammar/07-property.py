ac2c81252c2ee079b4ca2b0b50717c8dd11b9ffa
Armyz <armyz@foxmail.com>
2017-07-18 22:40:33 +0800

    修改07

--- learnPythonExample/pythonGrammar/07-property.py
+++ learnPythonExample/pythonGrammar/07-property.py
@@ -1,9 +1,11 @@
 
-'''
+"""
 property ：
 1.讲类方法变为转换为只读属性
 2.对类对象中的私有变量进行修改和值获取的一种方法,setter和getter方法
-'''
+"""
+
+
 class Test(object):
     def __init__(self):
         self.__num = 21
@@ -14,7 +16,7 @@ class Test(object):
         return self.__num
 
     @num.setter
-    def num(self,newNum):
+    def num(self, newNum):
         print("-----setter------")
         self.__num = newNum
 
