1aabbb209d99420ba696e650877e4a9b4772db84
Armyz <armyz@foxmail.com>
2017-07-19 23:28:08 +0800

    修改getattr属性的使用.py

--- learnPythonExample/pythonGrammar/022-getattr属性使用.py
+++ learnPythonExample/pythonGrammar/022-getattr属性使用.py
@@ -13,7 +13,7 @@ class Foo(object):
     def __str__(self):
         return ""
 
-# 在类查找类属性时，首先查找类属性中是否有该属性，若没有
-# 则类属性中没有则在__getattr__中是否是否有此属性。
+# 在类查找类属性时，首先查找实例字典foo.__dict__字典中是否有该属性，若没有
+# 则类的属性魔法属性Foo.__dict__字典中没有则最后在__getattr__中是否是否有此属性。
 # 在此可使用pycharm进行调试跟踪
 print(Foo().think.diffent.hello)
