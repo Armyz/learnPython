3327781841f346a4bece056ec5264175c1351b2f
Armyz <armyz@foxmail.com>
2017-07-19 23:21:35 +0800

    增加getattr属性的使用.py

---
+++ learnPythonExample/pythonGrammar/022-getattr属性使用.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+class Foo(object):
+    def __init__(self):
+        pass
+
+    def __getattr__(self, item):
+        print(item, "")
+        return self
+
+    # 打印时FOO对象时，默认的输出修改，若没有重写。则默认打印对象的地址
+    def __str__(self):
+        return ""
+
+# 在类查找类属性时，首先查找类属性中是否有该属性，若没有
+# 则类属性中没有则在__getattr__中是否是否有此属性。
+# 在此可使用pycharm进行调试跟踪
+print(Foo().think.diffent.hello)
