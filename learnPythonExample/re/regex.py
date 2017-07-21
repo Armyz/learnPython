c84526dad11bdf175f7058dd892adfa9366c6997
Armyz <armyz@foxmail.com>
2017-07-21 08:54:31 +0800

    modified re file

--- learnPythonExample/re/regex.py
+++ learnPythonExample/re/regex.py
@@ -7,6 +7,53 @@ fd_write = open("test-regex.txt", "w")
 
 read_lines = fd_read.readlines()
 
+# 匹配指定字符
+print("以下是固定字符串")
+for line in read_lines:
+    ret = re.match("itcast", line)
+    if ret is not None:
+        print(ret.group())
+
+# 找出手机号
+print("以下是手机号")
+for line in read_lines:
+    ret = re.match("([0-9]){11}", line)
+    if ret is not None:
+        print(ret.group())
+
+print("找出GET请求目录资源")
+for line in read_lines:
+    ret = re.search("\s/[\w.]*\s", line)
+    if ret is not None:
+        print(ret.group(0))
+
+# 匹配正确的163/qq/邮箱
+print("以下是163/qq邮箱号")
+for line in read_lines:
+    ret = re.match("\w{4,20}@(163|qq)\.com$", line)
+    if ret is not None:
+        print(ret.group())
+
+# 匹配正确的html格式字符串  \1表示匹配到的第一个分组中字符串()内的内容为一个分组
+# 以‘<’开头‘>’结尾
+print("匹配html格式字符串")
+for line in read_lines:
+    ret = re.match(r"^<([A-Za-z]+)>.*</\1>$", line)
+    if ret is not None:
+        print(ret.group())
+
+print("匹配<><>*</></>格式字符串,使用分组方法")
+for line in read_lines:
+    ret = re.match(r"^<(\w+)><(\w+)>.*</\2></\1>$", line)
+    if ret is not None:
+        print(ret.group())
+print ("匹配<><>*</></>格式字符串,使用分组启别名方法")
+for line in read_lines:
+    ret = re.match(r"^<(?P<name1>\w+)><(?P<name2>\w+)>.*</(?P=name2)></(?P=name1)>$", line)
+    if ret is not None:
+        print(ret.group())
+
+
 for line in read_lines:
     regex_return = re.sub(r"</?\w+>|&nbsp;", "", line)
     # print(regex_return)
@@ -30,3 +77,5 @@ for line in read_lines:
         print(regex_return.group())
         fd_write.writelines(regex_return.group())
         fd_write.write("\n")
+
+
